aa9bb540c96b7a1eb5074d4caae74c7badfa25cc
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-02-18 19:20:38 +0530

    creates /GET and /POST route for getting all books and adding a book respectively.
      adds missing dependencies.
      adds DB configuration and compose.yaml port mapping.

--- src/main/java/com/zolobooky/booky/BookyApplication.java
+++ src/main/java/com/zolobooky/booky/BookyApplication.java
@@ -1,13 +1,20 @@
 package com.zolobooky.booky;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookyApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/com/zolobooky/booky/books/BookEntity.java
@@ -0,0 +1,45 @@
+package com.zolobooky.booky.books;
+
+import com.zolobooky.booky.commons.CustomStatus.BOOK_STATUS;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import java.sql.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "books")
+@Table(name = "books",
+		uniqueConstraints = @UniqueConstraint(columnNames = { "book_name", "book_desc", "book_thumbnail" }))
+public class BookEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer book_id;
+
+	@Column(nullable = false)
+	private String book_name;
+
+	String book_desc;
+
+	@Column(nullable = false)
+	BOOK_STATUS book_status;
+
+	@Column(nullable = false, updatable = false)
+	Date book_next_available;
+
+	@Column(nullable = false)
+	private Double book_rating;
+
+	byte[] book_thumbnail;
+
+	Integer owner_id;
+
+}
