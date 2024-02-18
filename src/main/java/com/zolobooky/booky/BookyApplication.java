8d06aff9bbef6bf67ec49ed9d856789095a4a9ba
Kushagra S <kali@kali.teamkushagra.tech>
2024-02-18 12:02:42 +0530

    Initialized a skeleton for application

---
+++ src/main/java/com/zolobooky/booky/BookyApplication.java
@@ -0,0 +1,13 @@
+package com.zolobooky.booky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyApplication.class, args);
+	}
+
+}
