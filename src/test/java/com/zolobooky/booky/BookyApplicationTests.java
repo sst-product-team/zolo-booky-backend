c87fca3d08a9e0c72ffdbf77d1ea3ec6aab02783
Kushagra <kushagra.rigel@gmail.com>
2024-03-01 01:09:49 +0530

    Disabled MockMVC

--- src/test/java/com/zolobooky/booky/BookyApplicationTests.java
+++ src/test/java/com/zolobooky/booky/BookyApplicationTests.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@AutoConfigureMockMvc
 class BookyApplicationTests {
 
 }
