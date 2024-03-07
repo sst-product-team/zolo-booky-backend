b16f05fec6d197243de9631d3d2e4b3606b4fa58
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-08 00:50:20 +0530

    removes tests completely till added later.

--- src/test/java/com/zolobooky/booky/BookyApplicationTests.java
+++ src/test/java/com/zolobooky/booky/BookyApplicationTests.java
@@ -1,13 +1,5 @@
 package com.zolobooky.booky;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BookyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
