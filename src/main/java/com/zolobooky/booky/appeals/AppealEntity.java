a665758d6d490ccd871fbda1c6b940e808a18558
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-28 12:56:40 +0530

    removes unneeded dependencies.

--- src/main/java/com/zolobooky/booky/appeals/AppealEntity.java
+++ src/main/java/com/zolobooky/booky/appeals/AppealEntity.java
@@ -5,8 +5,6 @@ import com.zolobooky.booky.commons.CustomStatus;
 import com.zolobooky.booky.users.UserEntity;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import java.util.Date;
 
 @Data
