cf23fa83741922f378494bb964a641f1f717fc1d
AttAditya <attachment.aditya@gmail.com>
2024-04-04 03:04:22 +0530

    Added tests for [GET] endpoints of appeals. Update: Formatted Code.

--- src/test/java/com/zolobooky/booky/AppealAPITestAssets.java
+++ src/test/java/com/zolobooky/booky/AppealAPITestAssets.java
@@ -9,81 +9,87 @@ import java.sql.Date;
 import java.util.List;
 
 public class AppealAPITestAssets {
-    public UserEntity demoUser;
-    public BookEntity demoBook;
 
-    public final List<AppealEntity> appealEntityList;
+	public UserEntity demoUser;
 
-    public AppealEntity appealEntity1;
-    public AppealEntity appealEntity2;
-    public AppealEntity appealEntity3;
-    public AppealEntity appealEntity4;
+	public BookEntity demoBook;
 
-    public AppealAPITestAssets() {
-        demoUser = new UserEntity();
-        demoUser.setId(42);
-        demoUser.setName("Demo");
-        demoUser.setFcmToken("Demo-FCM-TOKEN");
+	public final List<AppealEntity> appealEntityList;
 
-        demoBook = new BookEntity();
-        demoBook.setId(900);
-        demoBook.setName("Test Book 2");
-        demoBook.setStatus(CustomStatus.BookStatus.AVAILABLE);
-        demoBook.setThumbnail("TEST IMAGE URL");
-        demoBook.setOwner(demoUser);
+	public AppealEntity appealEntity1;
 
-        appealEntity1 = new AppealEntity();
-        appealEntity1.setTrans_id(1);
-        appealEntity1.setTrans_status(CustomStatus.TransactionStatus.PENDING);
-        appealEntity1.setBook_id(demoBook);
-        appealEntity1.setBorrower_id(demoUser);
-        appealEntity1.setInitiation_date(new Date(1712174174));
-        appealEntity1.setCompletion_date(new Date(1712174174));
-        appealEntity1.setStatus_change_date(new Date(1712174174));
-        appealEntity1.setExpected_completion_date(new Date(1712174174));
+	public AppealEntity appealEntity2;
 
-        appealEntity2 = new AppealEntity();
-        appealEntity2.setTrans_id(2);
-        appealEntity2.setTrans_status(CustomStatus.TransactionStatus.PENDING);
-        appealEntity2.setBook_id(demoBook);
-        appealEntity2.setBorrower_id(demoUser);
-        appealEntity2.setInitiation_date(new Date(1712174174));
-        appealEntity2.setCompletion_date(new Date(1712174174));
-        appealEntity2.setStatus_change_date(new Date(1712174174));
-        appealEntity2.setExpected_completion_date(new Date(1712174174));
+	public AppealEntity appealEntity3;
 
-        appealEntity3 = new AppealEntity();
-        appealEntity3.setTrans_id(3);
-        appealEntity3.setTrans_status(CustomStatus.TransactionStatus.PENDING);
-        appealEntity3.setBook_id(demoBook);
-        appealEntity3.setBorrower_id(demoUser);
-        appealEntity3.setInitiation_date(new Date(1712174174));
-        appealEntity3.setCompletion_date(new Date(1712174174));
-        appealEntity3.setStatus_change_date(new Date(1712174174));
-        appealEntity3.setExpected_completion_date(new Date(1712174174));
+	public AppealEntity appealEntity4;
 
-        appealEntity4 = new AppealEntity();
-        appealEntity4.setTrans_id(4);
-        appealEntity4.setTrans_status(CustomStatus.TransactionStatus.PENDING);
-        appealEntity4.setBook_id(demoBook);
-        appealEntity4.setBorrower_id(demoUser);
-        appealEntity4.setInitiation_date(new Date(1712174174));
-        appealEntity4.setCompletion_date(new Date(1712174174));
-        appealEntity4.setStatus_change_date(new Date(1712174174));
-        appealEntity4.setExpected_completion_date(new Date(1712174174));
+	public AppealAPITestAssets() {
+		demoUser = new UserEntity();
+		demoUser.setId(42);
+		demoUser.setName("Demo");
+		demoUser.setFcmToken("Demo-FCM-TOKEN");
 
-        appealEntityList = List.of(appealEntity1, appealEntity2, appealEntity3, appealEntity4);
-    }
+		demoBook = new BookEntity();
+		demoBook.setId(900);
+		demoBook.setName("Test Book 2");
+		demoBook.setStatus(CustomStatus.BookStatus.AVAILABLE);
+		demoBook.setThumbnail("TEST IMAGE URL");
+		demoBook.setOwner(demoUser);
 
-    public List<AppealEntity> getAllAppeals() {
-        return this.appealEntityList;
-    }
+		appealEntity1 = new AppealEntity();
+		appealEntity1.setTrans_id(1);
+		appealEntity1.setTrans_status(CustomStatus.TransactionStatus.PENDING);
+		appealEntity1.setBook_id(demoBook);
+		appealEntity1.setBorrower_id(demoUser);
+		appealEntity1.setInitiation_date(new Date(1712174174));
+		appealEntity1.setCompletion_date(new Date(1712174174));
+		appealEntity1.setStatus_change_date(new Date(1712174174));
+		appealEntity1.setExpected_completion_date(new Date(1712174174));
 
-    public AppealEntity getAppeal() {
-        return this.appealEntity3;
-    }
+		appealEntity2 = new AppealEntity();
+		appealEntity2.setTrans_id(2);
+		appealEntity2.setTrans_status(CustomStatus.TransactionStatus.PENDING);
+		appealEntity2.setBook_id(demoBook);
+		appealEntity2.setBorrower_id(demoUser);
+		appealEntity2.setInitiation_date(new Date(1712174174));
+		appealEntity2.setCompletion_date(new Date(1712174174));
+		appealEntity2.setStatus_change_date(new Date(1712174174));
+		appealEntity2.setExpected_completion_date(new Date(1712174174));
 
-    public AppealEntity postAppeal() {
-        return this.appealEntity4;
-    }
+		appealEntity3 = new AppealEntity();
+		appealEntity3.setTrans_id(3);
+		appealEntity3.setTrans_status(CustomStatus.TransactionStatus.PENDING);
+		appealEntity3.setBook_id(demoBook);
+		appealEntity3.setBorrower_id(demoUser);
+		appealEntity3.setInitiation_date(new Date(1712174174));
+		appealEntity3.setCompletion_date(new Date(1712174174));
+		appealEntity3.setStatus_change_date(new Date(1712174174));
+		appealEntity3.setExpected_completion_date(new Date(1712174174));
+
+		appealEntity4 = new AppealEntity();
+		appealEntity4.setTrans_id(4);
+		appealEntity4.setTrans_status(CustomStatus.TransactionStatus.PENDING);
+		appealEntity4.setBook_id(demoBook);
+		appealEntity4.setBorrower_id(demoUser);
+		appealEntity4.setInitiation_date(new Date(1712174174));
+		appealEntity4.setCompletion_date(new Date(1712174174));
+		appealEntity4.setStatus_change_date(new Date(1712174174));
+		appealEntity4.setExpected_completion_date(new Date(1712174174));
+
+		appealEntityList = List.of(appealEntity1, appealEntity2, appealEntity3, appealEntity4);
+	}
+
+	public List<AppealEntity> getAllAppeals() {
+		return this.appealEntityList;
+	}
+
+	public AppealEntity getAppeal() {
+		return this.appealEntity3;
+	}
+
+	public AppealEntity postAppeal() {
+		return this.appealEntity4;
+	}
+
 }
--- src/test/java/com/zolobooky/booky/AppealServiceTest.java
+++ src/test/java/com/zolobooky/booky/AppealServiceTest.java
@@ -19,34 +19,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(AppealController.class)
 public class AppealServiceTest {
-    @Autowired
-    private MockMvc mockMvc;
 
-    @MockBean
-    private AppealService appealService;
+	@Autowired
+	private MockMvc mockMvc;
 
-    private final AppealAPITestAssets appealAPITestAssets = new AppealAPITestAssets();
+	@MockBean
+	private AppealService appealService;
 
-    @Test
+	private final AppealAPITestAssets appealAPITestAssets = new AppealAPITestAssets();
+
+	@Test
     void getAllAppealsTest() throws Exception {
         when(appealService.getAllAppeals()).thenReturn(appealAPITestAssets.getAllAppeals());
         mockMvc.perform(get("/v0/appeals")).andExpect(status().isOk());
     }
 
-    @Test
+	@Test
     void getAppealByIdTest() throws Exception {
         when(appealService.getAppeal(3)).thenReturn(appealAPITestAssets.getAppeal());
         mockMvc.perform(get("/v0/appeals/3")).andExpect(status().isOk());
     }
 
-    //@Test
-    void postAppealTest() throws Exception {
-        CreateAppealDTO createAppealDTO = new CreateAppealDTO(900, 42, new Date(2024, 4, 4));
-        when(appealService.createAppeal(createAppealDTO)).thenReturn(appealAPITestAssets.postAppeal());
+	// @Test
+	void postAppealTest() throws Exception {
+		CreateAppealDTO createAppealDTO = new CreateAppealDTO(900, 42, new Date(2024, 4, 4));
+		when(appealService.createAppeal(createAppealDTO)).thenReturn(appealAPITestAssets.postAppeal());
 
-        mockMvc.perform(post("/v0/appeals")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"book_id\": 900, \"borrower_id\": 42, \"expected_completion_date\": \"2024-04-04\"}")
-        ).andExpect(status().isOk());
-    }
+		mockMvc
+			.perform(post("/v0/appeals").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"book_id\": 900, \"borrower_id\": 42, \"expected_completion_date\": \"2024-04-04\"}"))
+			.andExpect(status().isOk());
+	}
+
 }
