a5dd59c7392552c4e6f11ee9e2e86a289fc4e70b
AttAditya <attachment.aditya@gmail.com>
2024-04-07 23:29:27 +0530

    Added tests for [POST] create book endpoint. Froze the appeal test for [PATCH] updating for now, since it has undetected bug.

--- src/test/java/com/zolobooky/booky/BookServiceTest.java
+++ src/test/java/com/zolobooky/booky/BookServiceTest.java
@@ -2,18 +2,20 @@ package com.zolobooky.booky;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zolobooky.booky.books.BookController;
+import com.zolobooky.booky.books.BookEntity;
 import com.zolobooky.booky.books.BookService;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.zolobooky.booky.books.dto.CreateBookDTO;
-import jdk.jfr.ContentType;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -47,6 +49,25 @@ public class BookServiceTest {
 	void delistBookSerivceTest() throws Exception {
 		when(bookService.deleteBook(0)).thenReturn(bookAPITestAssets.getDelistedBook());
 		this.mockMvc.perform(delete("/v0/books/0")).andExpect(status().isOk());
+	}
+
+	@Test
+	void createBookTest() throws Exception {
+		CreateBookDTO createBookDTO = new CreateBookDTO();
+		createBookDTO.setName("Demo Book DTO");
+		createBookDTO.setAuthor("Demo User");
+		createBookDTO.setDescription("This is a Demo Book");
+		createBookDTO.setMaxBorrow(1);
+		createBookDTO.setThumbnail("https://demo.book.url/");
+		createBookDTO.setOwner(1);
+
+		BookEntity bookEntity = bookAPITestAssets.createBook();
+		when(bookService.createBook(any(createBookDTO.getClass()))).thenReturn(bookEntity);
+
+		String payload = bookAPITestAssets.toJSONString(createBookDTO);
+
+		mockMvc.perform(post("/v0/books").contentType(MediaType.APPLICATION_JSON).content(payload))
+			.andExpect(status().isOk());
 	}
 
 }
